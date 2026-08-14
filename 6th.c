ae6c301b8e8f4f9dafdd15d76edadbf35f2e53ed
Student Rahul-kumbhar-CS <rahul886709@gmail.com>
2026-08-14 23:13:43 +0530

    Added 6th C program

---
+++ 6th.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char name[]="RAHUL";
+    char clg_name[]="BITM ballari";
+    char course[]="Data science";
+    printf("My name is %s\n",name);
+    printf("My college name is %s\n",clg_name);
+    printf("My course is %s\n",course);
+    return 0;
+}
