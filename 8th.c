1cce7fbc6e58674cec07275fc7ba4ee2917ebaa3
Student Rahul-kumbhar-CS <rahul886709@gmail.com>
2026-08-15 12:45:11 +0530

    8th program added

---
+++ 8th.c
@@ -0,0 +1,14 @@
+// to print student details
+#include<stdio.h>
+int main()
+{
+    char name[20]="Rahul b kumbhar";
+    int age=19;
+    char clg_name[]="BITM Ballari";
+    char branch[]="CSE(DS)";
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("College Name: %s\n", clg_name);
+    printf("Branch: %s\n", branch);
+    return 0;
+}
