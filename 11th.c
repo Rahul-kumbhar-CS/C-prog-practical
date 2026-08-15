ae2ae61f72106f001e8366f83529cf3c39eb6c71
Student Rahul-kumbhar-CS <rahul886709@gmail.com>
2026-08-15 22:59:07 +0530

    11th program added

---
+++ 11th.c
@@ -0,0 +1,11 @@
+//Quotient in whole Number When One Number Is Divided by Another.
+#include <stdio.h>
+int main()
+{
+    float a, b, quotient;
+    printf("Enetr the value of a and bl");
+    scanf("%f %f",&a,&b);
+    quotient=a/b;
+    printf("Quotient when %f is divided by %f is %f", a, b, quotient);
+    return 0;
+}
