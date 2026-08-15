0901857b08855ffd602095c85f5603582118a14b
Student Rahul-kumbhar-CS <rahul886709@gmail.com>
2026-08-15 12:27:09 +0530

    Added 7th c program

---
+++ 7th.c
@@ -0,0 +1,11 @@
+// sum of two numbers
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter two numbers\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("Sum of two numbers is %d\n",sum);
+    return 0;
+}
