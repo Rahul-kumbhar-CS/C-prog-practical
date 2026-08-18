76c50008c8bfb656f62141b3006dbdedc0b02ab2
Student Rahul-kumbhar-CS <rahul886709@gmail.com>
2026-08-18 21:23:48 +0530

    Add 22th program

---
+++ 26th.c
@@ -0,0 +1,12 @@
+//print all factors of a number
+#include<stdio.h>
+int main()
+{
+    int num, fact;
+    printf("Enter the number:");
+    scanf("%d",&num)
+    if(num%fact==0)
+        printf("Factors of a number:",fact)
+    return 0;
+}
+
